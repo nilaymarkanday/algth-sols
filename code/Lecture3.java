07771fdec0cb2c6cff7e5927a65168fee5ac08ce
kamasheto <me@sakr.me>
2014-10-29 14:06:57 +0100

    Solution to a (with dynamic programming)

--- code/Lecture3.java
+++ code/Lecture3.java
@@ -1,6 +1,11 @@
+import java.util.HashMap;
+
 public class Lecture3 {
   public static void main(String[] args) {
     list = new int[] {10, 2, 3, 4, 5};
+    p = new HashMap<String, Integer>();
+    g = new HashMap<String, Integer>();
+
     System.out.println(p(0, 2));
   }
 
@@ -11,13 +16,20 @@ public class Lecture3 {
       return list[2 * i];
     }
 
-    return g(i, j - 1) + list[2 * j - 1] + g(j, j);
+    String key = i + "|" + j;
+    if (g.containsKey(key)) return g.get(key);
+
+    g.put(key, g(i, j - 1) + list[2 * j - 1] + g(j, j));
+    return g.get(key);
   }
 
   public static int p(int i, int j) {
     if (i == j) {
       return 0;
     }
+
+    String key = i + "|" + j;
+    if (p.containsKey(key)) return p.get(key);
 
     int min = Integer.MAX_VALUE;
 
@@ -29,7 +41,9 @@ public class Lecture3 {
       }
     }
 
-    return g(i, j) + min;
-
+    p.put(key, g(i, j) + min);
+    return p.get(key);
   }
+
+  public static HashMap<String, Integer> p, g;
 }

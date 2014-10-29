9946f1aaad6727878cb0a3f4aa734a2f63f632f5
kamasheto <me@sakr.me>
2014-10-29 16:16:58 +0100

    Solution to 2)

--- code/Lecture2.java
+++ code/Lecture2.java
@@ -2,77 +2,36 @@ import java.util.HashMap;
 
 public class Lecture2 {
   public static void main(String[] args) {
-    int n = Integer.parseInt(args[0]), k = Integer.parseInt(args[1]);
-    System.out.println(partitions(n, k));
+    System.out.println(paths(Integer.parseInt(args[0])));
   }
 
-  public static long[] factorialCache;
-  public static long[][] binomialCache;
-  public static HashMap<String, Long> cache;
-
-  public static long partitions(int n, int k) {
-    factorialCache = new long[n + 1];
-    binomialCache = new long[n + 1][n + 1];
-    cache = new HashMap<String, Long>();
-    return g(1, n, k);
+  public static int paths(int n) {
+    cache = new HashMap<String, Integer>();
+    return paths(n, n);
   }
 
-  public static long g(int b, int n, int k) {
-    if (b < 0) {
-      return 0;
+  public static int paths(int x, int y) {
+    if (y < 0) return 0;
+    if (y > x) return 0;
+    if (x == 0) return 1;
+
+    String key = x + "|" + y;
+    if (cache.containsKey(key)) return cache.get(key);
+
+    int paths = 0;
+    for (int i = 0; i <= y; i++) {
+      paths += paths(x - 1, i);
     }
 
-    if (b == 0 || n == 0 || k < 2) {
-      return 1;
-    }
+    for (int j = 0; j <= x; j++) {
+      paths += paths(j, y - 1);
+    } 
 
-    String key = b + "|" + n + "|" + k;
+    paths -= paths(x - 1, y - 1);
 
-    if (cache.containsKey(key)) {
-      return cache.get(key);
-    }
-
-    if (b >= n) {
-      long sum = 0;
-      for (int t = 1; t <= n + 1; t++) {
-        int sign = t % 2 == 0 ? -1 : 1;
-        sum += binomial(n + 1, t) * g(b - t, n, k) * sign;
-      }
-
-      cache.put(key, sum);
-      return sum;
-    } else {
-      long r = g(b - 1, n, k) + g(b * k, n - 1, k);
-      cache.put(key, r);
-      return r;
-    }
+    cache.put(key, paths);
+    return paths;
   }
 
-  public static long binomial(int n, int k) {
-    if (binomialCache[n][k] != 0) {
-      return binomialCache[n][k];
-    }
-
-    long f = factorial(n) / (factorial(k) * factorial(n - k));
-    binomialCache[n][k] = f;
-    return f;
-  }
-
-  public static long factorial(int l) {
-
-    if (l == 0) 
-      return 1;
-
-    if (factorialCache[l] != 0) {
-      return factorialCache[l];
-    }
-
-    long f = l;
-    while (l-- > 1) {
-      f *= l;
-    }
-
-    factorialCache[l] = f;
-    return f;
-  }
+  public static HashMap<String, Integer> cache;
 }
